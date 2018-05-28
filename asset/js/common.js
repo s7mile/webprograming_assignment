ad9f2cc16720893ae0fe509e87d7c636d93c3d74
이민정 <haeru@iminjeong-ui-MacBook-Pro.local>
2018-05-28 19:13:33 +0900

    company 페이지 mouseover out 이벤트 추가

--- asset/js/common.js
+++ asset/js/common.js
@@ -2,8 +2,44 @@ function mainVisual(){
 	if(document.getElementById("firstVisual").className.match(/\bon\b/)){
 		document.getElementById("firstVisual").style.display = "none";
 		document.getElementById("firstVisual").classList.remove("on");
+		document.getElementById("mainvisual").style.backgroundColor = "#ed9fa2"
 	}else{
 		document.getElementById("firstVisual").style.display = "block";
 		document.getElementById("firstVisual").classList.add("on");
+		document.getElementById("mainvisual").style.backgroundColor = "#01bc78"
 	}
+}
+
+function overColor1(){
+	document.getElementById("sec2_1").style.borderColor = "#01bc78";
+	document.getElementById("sec2_1").style.fontWeight = "bold";
+}
+function overColor2(){
+	document.getElementById("sec2_2").style.borderColor = "#01bc78";
+	document.getElementById("sec2_2").style.fontWeight = "bold";
+}
+function overColor3(){
+	document.getElementById("sec2_3").style.borderColor = "#01bc78";
+	document.getElementById("sec2_3").style.fontWeight = "bold";
+}
+function overColor4(){
+	document.getElementById("sec2_4").style.borderColor = "#01bc78";
+	document.getElementById("sec2_4").style.fontWeight = "bold";
+}
+
+function outColor1(){
+	document.getElementById("sec2_1").style.borderColor = "#aaa";
+	document.getElementById("sec2_1").style.fontWeight = "normal";
+}
+function outColor2(){
+	document.getElementById("sec2_2").style.borderColor = "#aaa";
+	document.getElementById("sec2_2").style.fontWeight = "normal";
+}
+function outColor3(){
+	document.getElementById("sec2_3").style.borderColor = "#aaa";
+	document.getElementById("sec2_3").style.fontWeight = "normal";
+}
+function outColor4(){
+	document.getElementById("sec2_4").style.borderColor = "#aaa";
+	document.getElementById("sec2_4").style.fontWeight = "normal";
 }
