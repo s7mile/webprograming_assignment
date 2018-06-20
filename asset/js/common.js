13d4d4e6361bca1b209f4a7c26d7c69b04ca24ec
이민정 <haeru@iminjeong-ui-MacBook-Pro.local>
2018-06-20 14:46:13 +0900

    jquery 1차 적용

--- asset/js/common.js
+++ asset/js/common.js
@@ -1,48 +1,75 @@
-function mainVisual(){
-	if(document.getElementById("firstVisual").className.match(/\bon\b/)){
-		document.getElementById("firstVisual").style.display = "none";
-		document.getElementById("firstVisual").classList.remove("on");
-		document.getElementById("mainvisual").style.backgroundColor = "#ed9fa2"
-	}else{
-		document.getElementById("firstVisual").style.display = "block";
-		document.getElementById("firstVisual").classList.add("on");
-		document.getElementById("mainvisual").style.backgroundColor = "#01bc78"
-	}
-}
+$(function(){
+	//mainvisual
+	$("#leftMoveBtn, #rightMoveBtn").on("click", function(){
+		if($("#firstVisual").hasClass("on")){
+			$("#firstVisual").stop().fadeOut().removeClass("on");
+			$("#secVisual").stop().fadeIn();
+			$("#mainvisual").css({"background": "#ed9fa2"});
+		}else{
+			$("#firstVisual").stop().fadeIn().addClass("on");
+			$("#secVisual").stop().fadeOut();
+			$("#mainvisual").css({"background": "#01bc78"});
+		}
+	});
 
-function overColor1(){
-	document.getElementById("sec2_1").style.borderColor = "#01bc78";
-	document.getElementById("sec2_1").style.fontWeight = "bold";
-}
-function overColor2(){
-	document.getElementById("sec2_2").style.borderColor = "#01bc78";
-	document.getElementById("sec2_2").style.fontWeight = "bold";
-}
-function overColor3(){
-	document.getElementById("sec2_3").style.borderColor = "#01bc78";
-	document.getElementById("sec2_3").style.fontWeight = "bold";
-}
-function overColor4(){
-	document.getElementById("sec2_4").style.borderColor = "#01bc78";
-	document.getElementById("sec2_4").style.fontWeight = "bold";
-}
+	//menu.html tabmenu
+	$("#menu .tabmenu a").on("click", function(){
+		$("#menu article").hide();
+		$("#" + $(this).attr("data-type")).slideDown();
+	});
 
-function outColor1(){
-	document.getElementById("sec2_1").style.borderColor = "#aaa";
-	document.getElementById("sec2_1").style.fontWeight = "normal";
-}
-function outColor2(){
-	document.getElementById("sec2_2").style.borderColor = "#aaa";
-	document.getElementById("sec2_2").style.fontWeight = "normal";
-}
-function outColor3(){
-	document.getElementById("sec2_3").style.borderColor = "#aaa";
-	document.getElementById("sec2_3").style.fontWeight = "normal";
-}
-function outColor4(){
-	document.getElementById("sec2_4").style.borderColor = "#aaa";
-	document.getElementById("sec2_4").style.fontWeight = "normal";
-}
+	//company.html hover
+	$("#sec2 li").hover(function(){
+		$(this).css({"border-color": "#01bc78", "font-weight": "bold"});
+	}, function(){
+		$(this).css({"border-color": "#aaa", "font-weight": "normal"});
+	});
+});
+// function mainVisual(){
+// 	if(document.getElementById("firstVisual").className.match(/\bon\b/)){
+// 		document.getElementById("firstVisual").style.display = "none";
+// 		document.getElementById("firstVisual").classList.remove("on");
+// 		document.getElementById("mainvisual").style.backgroundColor = "#ed9fa2"
+// 	}else{
+// 		document.getElementById("firstVisual").style.display = "block";
+// 		document.getElementById("firstVisual").classList.add("on");
+// 		document.getElementById("mainvisual").style.backgroundColor = "#01bc78"
+// 	}
+// }
+
+// function overColor1(){
+// 	document.getElementById("sec2_1").style.borderColor = "#01bc78";
+// 	document.getElementById("sec2_1").style.fontWeight = "bold";
+// }
+// function overColor2(){
+// 	document.getElementById("sec2_2").style.borderColor = "#01bc78";
+// 	document.getElementById("sec2_2").style.fontWeight = "bold";
+// }
+// function overColor3(){
+// 	document.getElementById("sec2_3").style.borderColor = "#01bc78";
+// 	document.getElementById("sec2_3").style.fontWeight = "bold";
+// }
+// function overColor4(){
+// 	document.getElementById("sec2_4").style.borderColor = "#01bc78";
+// 	document.getElementById("sec2_4").style.fontWeight = "bold";
+// }
+
+// function outColor1(){
+// 	document.getElementById("sec2_1").style.borderColor = "#aaa";
+// 	document.getElementById("sec2_1").style.fontWeight = "normal";
+// }
+// function outColor2(){
+// 	document.getElementById("sec2_2").style.borderColor = "#aaa";
+// 	document.getElementById("sec2_2").style.fontWeight = "normal";
+// }
+// function outColor3(){
+// 	document.getElementById("sec2_3").style.borderColor = "#aaa";
+// 	document.getElementById("sec2_3").style.fontWeight = "normal";
+// }
+// function outColor4(){
+// 	document.getElementById("sec2_4").style.borderColor = "#aaa";
+// 	document.getElementById("sec2_4").style.fontWeight = "normal";
+// }
 
 function loginForm(){
 	if(fr.userid.value == "") {
